adc16be415a6145815fb1e47dceca0a0a509ae23
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-05-30 05:13:48 +0530

    Installing react and fetch data using custom hook

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import {verifyAdmin} from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -12,8 +12,10 @@ router.put("/:id",verifyAdmin, updateHotel);
 // DELETE
 router.delete("/:id",verifyAdmin, deleteHotel);
 // GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 // GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 
 export default router;
