664ec1f936a783212d5620f39fbad9a0efe1cff6
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-06-06 18:31:04 +0530

    Updated and featuredproperty is not appearing

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,23 @@
-import "./navbar.css"
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Mushbooking</span>
+        <Link to="/" style={{
+          color: "inherit",
+          textDecoration: "none",
+        }}>
+          <span className="logo">Mushbooking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { SearchContextProvider } from "../src/context/SearchContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <SearchContextProvider>
       <App />
+    </SearchContextProvider>
   </React.StrictMode>
 );
-
-
